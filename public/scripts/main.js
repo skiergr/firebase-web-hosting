3953c7dcd8b1f899e2f8bdca709edd41123adc0a
Grady <gradyramberg@gmail.com>
2023-10-21 15:19:38 -0700

    Event Listeners and other stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,28 +1,85 @@
 class Main {
-    constructor() {
-        this.pageViewsKey = 'pageViewsCount';
-        this.initializeCounter();
-        this.displayCount();
+  constructor() {
+    this.pageViewsKey = 'pageViewsCount';
+    this.currentPageKey = 'currentPage';
+
+    // Initialize the current page if it's not set
+    if (!localStorage.getItem(this.currentPageKey)) {
+      localStorage.setItem(this.currentPageKey, 'about.html');
     }
 
-    initializeCounter() {
-        if (!localStorage.getItem(this.pageViewsKey)) {
-            localStorage.setItem(this.pageViewsKey, '0');
-        }
-    }
+    this.initializeCounter();
+    this.displayCount();
 
-    incrementCount() {
-        let currentCount = parseInt(localStorage.getItem(this.pageViewsKey));
-        currentCount++;
-        localStorage.setItem(this.pageViewsKey, currentCount.toString());
-    }
+    // Handle the button click
+    document.getElementById('playMusicButton').addEventListener('click', () => {
+      this.playMusicAndFlash();
+    });
 
-    displayCount() {
-        this.incrementCount();
-        // Update count in div id count
-        document.getElementById('count').innerHTML = 'You have visited this page ' + localStorage.getItem(this.pageViewsKey)  + ' times.'
+    //event listener responding to any click
+    document.addEventListener('click', (event) => {
+      //confirm element is not a button
+      if (event.target.tagName === 'BUTTON') {
+        return;
+      }
+      //change background color to a random color
+      this.changeBackgroundColor(
+        '#' + Math.floor(Math.random() * 16777215).toString(16)
+      );
+    });
+  }
+
+  initializeCounter() {
+    if (!localStorage.getItem(this.pageViewsKey)) {
+      localStorage.setItem(this.pageViewsKey, '0');
     }
+  }
+
+  incrementCount() {
+    let currentCount = parseInt(localStorage.getItem(this.pageViewsKey));
+    currentCount++;
+    localStorage.setItem(this.pageViewsKey, currentCount.toString());
+  }
+
+  displayCount() {
+    this.incrementCount();
+    // Update count in div id count
+    document.getElementById('count').innerHTML =
+      'You have visited this page ' +
+      localStorage.getItem(this.pageViewsKey) +
+      ' times.';
+  }
+
+  playMusicAndFlash() {
+    // Play music
+    const audio = new Audio('Wii Music - Background Music.mp3');
+    audio.play();
+
+    // Flash the screen white
+    document.body.style.backgroundColor = 'white';
+
+    // change background color to a random color after .5 sceonds
+    setTimeout(() => {
+      //change color to black
+      this.changeBackgroundColor('black');
+    }, 250);
+
+    setTimeout(() => {
+      this.changeBackgroundColor('white');
+    }, 250);
+
+    setTimeout(() => {
+      this.changeBackgroundColor(
+        '#' + Math.floor(Math.random() * 16777215).toString(16)
+      );
+    }, 250);
+  }
+
+  //method that changes the page background color
+  changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+  }
 }
 
-// Note that we construct the class here, but we don't need to assign it to a variable.
+// Create an instance of the Main class
 document.mainClass = new Main();
